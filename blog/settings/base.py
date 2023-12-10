a8394afb4ed82bb92a3fbc617b1755ff840752be
Liliana Marozzi <lilimarozzi@gmail.com>
2023-12-10 20:18:35 -0300

    mjs

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -71,20 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'lili3239',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
