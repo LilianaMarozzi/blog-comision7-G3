a4182ef5c9d1458e1d686004146737d62e3dcee4
Liliana Marozzi <lilimarozzi@gmail.com>
2023-12-14 19:21:29 -0300

    modificar publicacion completo

--- publicaciones/urls.py
+++ publicaciones/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from.import views
+from . import views
 
 urlpatterns = [
-    path("ver-publicaciones/", views.publicaciones_view, name="publicaciones")
+    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones'),
+    path('publicar/', views.PublicarView.as_view(), name='publicar'),
+    path('modificar/<int:pk>', views.ModificarPublicacionView.as_view(), name='modificar-publicacion')
 ]
-
