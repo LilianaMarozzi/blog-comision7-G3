e7f3445bfc03012b60106fa907e07f910f59d2a8
Florencia Short <flor.r_s@hotmail.com>
2023-12-14 01:27:01 -0300

    Creando forms para las funciones de crear y modificar. Creo las funciones para publicar y modificar publicaciones. Views basadas en clases.

---
+++ publicaciones/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Publicacion
+
+class PublicarForm(forms.ModelForm):
+    class Meta:
+        model = Publicacion
+        fields = ['titulo', 'cuerpo', 'categoria', 'creador']
+
--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Publicacion
-
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import PublicarForm
+"""
 # Views basada en funciones para enlistar las publicaciones
 def publicaciones_view (request):
 
@@ -9,3 +11,21 @@ def publicaciones_view (request):
     }
 
     return render (request, 'publicaciones.html', ctx)    
+"""
+# View basada en clase para ENLISTAR publicaciones
+class PublicacionesView(ListView):
+    model = Publicacion
+    template_name = 'publicaciones/publicaciones.html'
+    context_object_name = 'publicaciones'
+
+# View basada en clase para CREAR publicaciones
+class PublicarView(CreateView):
+    model = Publicacion
+    template_name = 'publicaciones/publicar.html'
+    form_class = PublicarForm
+
+class ModificarPublicacionView(UpdateView):
+    model = Publicacion
+    template_name = 'publicaciones/modificar-publicacion.html'
+    form_class = PublicarForm
+    success_url = '../ver-publicaciones'
